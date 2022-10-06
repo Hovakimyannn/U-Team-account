fcaa421bc36b3cd7295cdfe5041fa8d30995fb99
MilSimonyan <millsimonyan01@gmail.com>
2022-10-06 15:10:21 +0400

    add student relation

--- database/migrations/2022_10_06_094821_create_students_table.php
+++ database/migrations/2022_10_06_094821_create_students_table.php
@@ -22,6 +22,7 @@ return new class extends Migration {
             $table->string('password');
             $table->unsignedBigInteger('department_id');
             $table->unsignedBigInteger('course_id');
+            $table->foreign('course_id')->references('id')->on('courses');
             $table->timestamps();
         });
     }
