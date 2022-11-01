81349aec0b2c7f1e1ec85b005c5b4f831d7e3b74
MilSimonyan <millsimonyan01@gmail.com>
2022-11-01 18:20:15 +0400

    fix

--- app/Http/Controllers/PasswordController.php
+++ app/Http/Controllers/PasswordController.php
@@ -5,20 +5,22 @@ namespace App\Http\Controllers;
 use Illuminate\Auth\Events\PasswordReset;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Password;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Str;
-use Illuminate\Testing\Fluent\Concerns\Has;
 
 class PasswordController extends Controller
 {
+    /**
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function send(Request $request) : JsonResponse
     {
-
         $validator = Validator::make($request->all(), [
-            'email'      => 'required|email',
+            'email' => 'required|email',
         ]);
 
         if ($validator->fails()) {
@@ -29,33 +31,37 @@ class PasswordController extends Controller
             $request->only('email')
         );
 
-        if($status === 'passwords.user') {
+        if ($status === 'passwords.user') {
             $status = Password::RESET_LINK_SENT;
         }
 
         if ($status == Password::RESET_LINK_SENT) {
             return new JsonResponse(['message' => trans($status)], JsonResponse::HTTP_OK);
         }
+
         return new JsonResponse(['message' => trans($status)], JsonResponse::HTTP_UNAUTHORIZED);
     }
 
-    public function reset(Request $request)
+    /**
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function reset(Request $request) : JsonResponse
     {
         $validator = Validator::make($request->all(), [
-            'token'                => 'required',
-            'email'                => 'required|exists:password_resets,email',
-            'password'             => 'required|confirmed',
+            'token'    => 'required',
+            'email'    => 'required|exists:password_resets,email',
+            'password' => 'required|confirmed',
         ]);
 
-        if($validator->fails())
-        {
+        if ($validator->fails()) {
             return new JsonResponse($validator->errors(), JsonResponse::HTTP_BAD_REQUEST);
         }
 
         $status = Password::reset(
             $request->only('email', 'password', 'token'),
-            function ($user) use ($request): void
-            {
+            function ($user) use ($request) : void {
                 $user->forceFill([
                     'password' => Hash::make($request->get('password'))
                 ])->setRememberToken(Str::random(60));
@@ -67,9 +73,9 @@ class PasswordController extends Controller
         );
 
         if ($status === Password::PASSWORD_RESET) {
-            return new JsonResponse(['message' => trans($status)], Response::HTTP_OK);
+            return new JsonResponse(['message' => trans($status)], JsonResponse::HTTP_OK);
         }
 
-        return new JsonResponse(['message' => trans($status)], Response::HTTP_UNAUTHORIZED);
+        return new JsonResponse(['message' => trans($status)], JsonResponse::HTTP_UNAUTHORIZED);
     }
 }
