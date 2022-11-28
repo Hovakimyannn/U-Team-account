e1d8743874a481d0c94f399d36b9bbccc2ad0696
Hovakimyannn <harutyunhovakimyan7@gmail.com>
2022-11-28 11:08:55 +0400

    delete unused

--- app/Http/Controllers/StudentInvitationController.php
+++ app/Http/Controllers/StudentInvitationController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Traits\Invitation;
 use App\Models\StudentInvitation;
 use App\Repositories\StudentInvitationRepository;
-use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
