b11a8027abbe3f4df2c92d565407f2064f5ad8a7
Hovakimyannn <harutyunhovakimyan7@gmail.com>
2023-05-04 01:23:29 +0400

    fix blade

--- resources/views/invitation.blade.php
+++ resources/views/invitation.blade.php
@@ -1,58 +1,15 @@
-<x-mail::message>
-    {{-- Greeting --}}
-    @if (! empty($greeting))
-        # {{ $greeting }}
-    @else
-        @if ($level === 'error')
-            # @lang('Whoops!')
-        @else
-            # @lang('Hello ') . {{$name . '!'}}
-        @endif
-    @endif
-
-    {{-- Intro Lines --}}
-    @foreach ($introLines as $line)
-        {{ $line }}
-
-    @endforeach
-
-    {{-- Action Button --}}
-    @isset($actionText)
-            <?php
-            $color = match ($level) {
-                'success', 'error' => $level,
-                default => 'primary',
-            };
-            ?>
-        <x-mail::button :url="$actionUrl" :color="$color">
-            {{ $actionText }}
-        </x-mail::button>
-    @endisset
-
-    {{-- Outro Lines --}}
-    @foreach ($outroLines as $line)
-        {{ $line }}
-
-    @endforeach
-
-    {{-- Salutation --}}
-    @if (! empty($salutation))
-        {{ $salutation }}
-    @else
-        @lang('Regards'),<br>
-        {{ config('app.name') }}
-    @endif
-
-    {{-- Subcopy --}}
-    @isset($actionText)
-        <x-slot:subcopy>
-            @lang(
-                "If you're having trouble clicking the \":actionText\" button, copy and paste the URL below\n".
-                'into your web browser:',
-                [
-                    'actionText' => $actionText,
-                ]
-            ) <span class="break-all">[{{ $displayableActionUrl }}]({{ $actionUrl }})</span>
-        </x-slot:subcopy>
-    @endisset
-</x-mail::message>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{{ $title }}</title>
+</head>
+<body>
+<h1>{{ $title }}</h1>
+<p>Hello {{ $name }},</p>
+<p>You are invited to join our website!</p>
+<p>Please click the following link to create your account:</p>
+<a href="{{ $invitationLink }}">{{ $invitationLink }}</a>
+</body>
+</html>
